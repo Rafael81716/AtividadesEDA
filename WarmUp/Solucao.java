599ebba5c61602a8f3a3d0a8e18b46866ba2f9bc
Rafael Cavalcante <rafacavalcante081@gmail.com>
2022-09-27 10:24:42 -0300

    resolução de Warm Up

--- WarmUp/Solucao.java
+++ WarmUp/Solucao.java
@@ -1,49 +1,30 @@
-package AtividadesEDA.WarmUp;
-/* 
-import java.util.Scanner;
-
-public class Solucao {
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int multiplier = sc.nextInt();
-        String values = sc.nextLine();
-
-        String[] stringValues = values.split(" ");
-        int[] finalValues = new int[stringValues.length];
-        int[] intValues = new int[stringValues.length];
-
-        for(int i = 0; i < intValues.length; i++){
-            intValues[i] = Integer.parseInt(stringValues[i]);
-        }
-
-        for(int i = 0; i < finalValues.length; i++){
-            finalValues[i] = intValues[i] * multiplier;
-        }
-
-        System.out.println(finalValues);
-    }
-}
-*/
+package WarmUp;
 import java.util.Scanner;
 
 class Main{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        Integer multiplier = sc.nextInt();
+        int multiplier = sc.nextInt();
+        sc.nextLine();
         String values = sc.nextLine();
 
         String[] stringValues = values.split(" ");
-        int[] finalValues = new int[stringValues.length];
-        int[] intValues = new int[stringValues.length];
 
-        for(int i = 0; i < intValues.length; i++){
+        int[] intValues = new int[stringValues.length];
+        for(int i = 0; i < stringValues.length; i++){
             intValues[i] = Integer.parseInt(stringValues[i]);
         }
 
-        for(int i = 0; i < finalValues.length; i++){
+        int[] finalValues = new int[intValues.length];
+        for(int i = 0; i < intValues.length; i++){
             finalValues[i] = intValues[i] * multiplier;
         }
+        
+        String output = "";
+        for(int i = 0; i < finalValues.length; i++){
+            output += finalValues[i] + " ";
+        }
 
-        System.out.println(finalValues);
+        System.out.println(output);
     }
 }
