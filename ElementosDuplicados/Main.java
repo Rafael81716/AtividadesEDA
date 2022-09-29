6556771f64b188e9909571edf53f4d5cf7431059
Rafael Cavalcante <rafacavalcante081@gmail.com>
2022-09-29 12:49:14 -0300

    Warm up com sucesso e melhorias em elementos duplicados

--- ElementosDuplicados/Main.java
+++ ElementosDuplicados/Main.java
@@ -3,16 +3,12 @@ import java.util.Scanner;
 class Main{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        String entry = sc.nextLine();
-        String[] stringValues = entry.split(" ");
-        int[] intArray = new int[stringValues.length];
-        for(int i = 0; i < intArray.length; i++){
-            intArray[i] = Integer.parseInt(stringValues[i]);
-        }
+        String[] entry = sc.nextLine().split(" ");
+    
         boolean retorno = false;
-        for(int i = 0; i < intArray.length; i++){
-            for(int j = 0; j < intArray.length; j++){
-                if(intArray[i] == intArray[j] && i != j){
+        for(int i = 0; i < entry.length; i++){
+            for(int j = i + 1; j < entry.length; j++){
+                if(entry[i].equals(entry[j])){
                     retorno = true;
                 }
             }
--- WarmUp/Solucao.java
+++ WarmUp/Solucao.java
@@ -1,34 +1,17 @@
 package WarmUp;
 import java.util.Scanner;
 
-public class Solucao{
+class Solucao{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        int multiplier = sc.nextInt();
-        sc.nextLine();
-        String values = sc.nextLine();
-
-        String[] stringValues = values.split(" ");
-
-        int[] intValues = new int[stringValues.length];
-        for(int i = 0; i < stringValues.length; i++){
-            intValues[i] = Integer.parseInt(stringValues[i]);
-        }
-
-        int[] finalValues = new int[intValues.length];
-        for(int i = 0; i < intValues.length; i++){
-            finalValues[i] = intValues[i] * multiplier;
-        }
+        int multiplier = Integer.parseInt(sc.nextLine());
+        String[] values = sc.nextLine().split(" ");
         
         String output = "";
-        for(int i = 0; i < finalValues.length; i++){
-            if(i == finalValues.length - 1){
-                output += finalValues[i] + "\n";
-            }else{
-                output += finalValues[i] + " ";
-            }
+        for(String s: values){
+            output += Integer.parseInt(s) * multiplier + " ";
         }
 
-        System.out.println(output);
+        System.out.println(output.trim());
     }
 }
