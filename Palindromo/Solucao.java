abcac07dd0b8fce4975ef609d9278a20090c2674
Rafael Cavalcante <rafacavalcante081@gmail.com>
2022-10-05 12:52:44 -0300

    sc.close nas classes antigas e finalizado Troca Vizinho (falta fazer o tempo de execução)

--- Palindromo/Solucao.java
+++ Palindromo/Solucao.java
@@ -2,15 +2,33 @@ package Palindromo;
 
 import java.util.Scanner;
 
-import javax.swing.text.PlainDocument;
-
 public class Solucao {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        
+        String word = sc.nextLine();
+        boolean output = eh_palindromo(word.toCharArray());
+        System.out.println(output);
+        sc.close();
     }
 
-    public boolean eh_palindromo(char[] palavra){
-        return false;
+    public static boolean eh_palindromo(char[] palavra){
+    
+        String word = "";
+        for (char p: palavra){
+            word += p;
+        }
+        String inverted = new StringBuffer(word).reverse().toString();
+        if(inverted.equals(word)){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
+/*
+ * Tempo de execução
+ * 
+ * 
+ * 
+ * Ordem de crescimento
+ */
